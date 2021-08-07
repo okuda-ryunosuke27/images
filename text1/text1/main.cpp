ed5fca02d9deda2e1025783e834d99e3f0f1e552
BASANAI\okuda-r <okuda-r@kbcgroup.ac.jp>
2021-08-07 17:37:43 +0900

    no message

---
+++ text1/text1/main.cpp
@@ -0,0 +1,94 @@
+#include "DxLib.h"
+
+const char TITLE[] = "TestProgram";
+
+//ウィンドウサイズ
+const int WIN_WIDTH = 640;
+const int WIN_HEIGHT = 480;
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	//ウィンドウモードに設定
+	ChangeWindowMode(TRUE);
+
+	/*ウィンドウサイズを手動では変更せず、
+	ウィンドウサイズに併せて拡大できないようにする*/
+	SetWindowSizeChangeEnableFlag(FALSE, FALSE);
+
+	//タイトルを変更
+	SetMainWindowText(TITLE);
+
+	//画面サイズの最大サイズ,カラービット数を設定(モニターの解像度に合わせる)
+	SetGraphMode(WIN_WIDTH, WIN_HEIGHT, 32);
+
+	//画像サイズを設定(解像度との比率で設定)
+	SetWindowSizeExtendRate(1.0);
+
+	//画面の背景色を設定する
+	SetBackgroundColor(0x00, 0x00, 0xAF);
+
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1)
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	//(ダブルバッファ)描画先グラフィック領域は裏面を指定
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//画像などのリソースデータの変数宣言と読み込み
+	int title = LoadGraph("title.png");
+	int coin = LoadGraph("hatena0.png");
+	//ゲームループで使う変数の宣言
+
+	//最新のキーボード情報用
+	char keys[256] = { 0 };
+
+	//1ループ(フレーム)前のキーボード情報
+	char oldkeys[256] = { 0 };
+
+	//ゲームループ
+	while (1)
+	{
+		//最新のキーボード情報だったものは1フレーム前のキーボード情報として保存
+		for (int i = 0; i < 256; ++i)
+		{
+			oldkeys[i] = keys[i];
+		}
+		//最新のキーボード情報を取得
+		GetHitKeyStateAll(keys);
+
+		//画面クリア
+		ClearDrawScreen();
+
+		//更新処理
+
+
+		//描画処理
+		DrawGraph(0, 0, title, true);
+		
+
+		//(ダブルバッファ)裏面
+		ScreenFlip();
+
+		//20ミリ秒待機(疑似60FPS)
+		WaitTimer(20);
+
+		//Windowsシステムからくる情報を処理する
+		if (ProcessMessage() == -1)
+		{
+			break;
+		}
+
+		//ESCキーが押されたらループから抜ける
+		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1)
+		{
+			break;
+		}
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
